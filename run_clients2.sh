a57d4a78237e38cf8b88c8800080833d5cd14e91
alejoriosm04 <alejo.rios.munoz04@gmail.com>
2024-10-14 00:50:41 -0500

    chore: Refactor network creation and configuration in run_clients2.sh and stop_clients2.sh

--- run_clients2.sh
+++ run_clients2.sh
@@ -5,19 +5,14 @@ sudo docker stop dhcp_server dhcp_relay dhcp_client1 dhcp_client2
 sudo docker rm dhcp_server dhcp_relay dhcp_client1 dhcp_client2
 
 # Eliminar redes existentes
-sudo docker network rm dhcp_net subnet_b
+sudo docker network rm dhcp_net_a dhcp_net_b
+
+sudo docker build -t dhcp_client -f Dockerfile.client .
+sudo docker build -t dhcp_server -f Dockerfile.server .
+sudo docker build -t dhcp_relay -f Dockerfile.relay .
 
 # Crear redes
-sudo docker network create --subnet=192.168.1.0/24 dhcp_net
-sudo docker network create --subnet=192.168.2.0/24 subnet_b
+sudo docker network create --subnet=192.168.1.0/24 dhcp_net_a
+sudo docker network create --subnet=192.168.2.0/24 dhcp_net_b
 
-# Ejecutar servidor DHCP
-sudo docker run -d --name dhcp_server --net subnet_b --ip 192.168.2.2 --cap-add=NET_ADMIN dhcp_server
-
-# Ejecutar DHCP Relay
-sudo docker run -d --name dhcp_relay --net dhcp_net --ip 192.168.1.2 --cap-add=NET_ADMIN dhcp_relay
-sudo docker network connect subnet_b dhcp_relay --ip 192.168.2.3
-
-# Ejecutar clientes DHCP
-sudo docker run -it --name dhcp_client1 --net dhcp_net --cap-add=NET_ADMIN dhcp_client
-sudo docker run -it --name dhcp_client2 --net subnet_a --cap-add=NET_ADMIN dhcp_client
+sudo docker run --privileged -d dhcp_relay bash && container_id=$(sudo docker ps -lq) && sudo docker exec -it $container_id bash -c "echo 1 > /proc/sys/net/ipv4/ip_forward"
--- stop_clients2.sh
+++ stop_clients2.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 # Detener y eliminar contenedores
-sudo docker stop dhcp_server dhcp_relay dhcp_client1 dhcp_client2
-sudo docker rm dhcp_server dhcp_relay dhcp_client1 dhcp_client2
+sudo docker stop dhcp_server dhcp_relay dhcp_client1 dhcp_client2 dhcp_client3
+sudo docker rm dhcp_server dhcp_relay dhcp_client1 dhcp_client2 dhcp_client3
 
 # Eliminar redes
-sudo docker network rm dhcp_net subnet_b
+sudo docker network rm dhcp_net_a dhcp_net_b
